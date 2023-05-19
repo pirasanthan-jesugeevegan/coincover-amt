ce2fb17b24562111a7c8c9b0d83bb19a55857a7e
Pirasanthan Jesugeevegan <pirasanthan.jesugeevegan@CC0092.local>
2023-05-19 12:53:40 +0100

    add report to s3

--- deploy-report.sh
+++ deploy-report.sh
@@ -8,7 +8,3 @@ aws s3 sync \
     --acl public-read \
     ./playwright-report \
     s3://coincover/ui/${date=$(date '+%Y-%m-%d')}
-upload_code=$?
-
-# All 3 commands should have succeeded (code=0) for the script to be consider successful
-exit $((upload_code))
