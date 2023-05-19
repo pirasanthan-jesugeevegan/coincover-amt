b80b1a74834d4749f116cd248b5bbcfb04148b5e
Pirasanthan Jesugeevegan <pirasanthan.jesugeevegan@CC0092.local>
2023-05-19 12:16:20 +0100

    fixed command fail

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,6 +11,7 @@ import { devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 const config: PlaywrightTestConfig = {
+  testDir: './tests',
   testMatch: ['**/tests/*.ui.ts', '**/tests/*.api.ts'],
   /* Maximum time one test can run for. */
   timeout: 30 * 1000,
@@ -30,7 +31,7 @@ const config: PlaywrightTestConfig = {
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'allure-playwright',
+  reporter: 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
