d3d2a741c404ecac5475f737934f62b5bce9d6fa
Pirasanthan Jesugeevegan <pirasanthan.jesugeevegan@CC0092.local>
2023-05-19 14:39:43 +0100

    add report to s3

--- deploy-report.sh
+++ deploy-report.sh
@@ -7,4 +7,4 @@ set +e
 aws s3 sync \
     --acl public-read \
     ./playwright-report \
-    s3://coincover-pj/ui/${date=$(date '+%Y-%m-%d')}
+    s3://coincover-pj/${TEST_TYPE}/${date=$(date '+%Y-%m-%d')}
